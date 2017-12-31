99220c4495b7466075df7f3c2682b98131e9f312
KHA <18017462799@163.com>
2017-12-31 16:29:20 +0800

    Initial Commit

---
+++ Final Reversi/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Final Reversi
+//
+//  Created by Khazeus on 31/12/2017.
+//  Copyright © 2017 Khazeus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
